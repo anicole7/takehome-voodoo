0081d74ebf4c8be898e2857658c2b74737e4c5f3
NICOLE Antoine <nicoleantoine@MBP-de-Antoine.lan>
2023-02-15 10:30:58 +0100

    populate function + front update

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,4 +1,7 @@
 class Api::GamesController < ActionController::API
+  require 'uri'
+  require 'net/http'
+
   def index
     render json: Game.all
   end
@@ -22,7 +25,7 @@ class Api::GamesController < ActionController::API
   def search
     game_list = []
     # get non empty fields
-    params_check = {name: params[:name], platform: params[:platform]}.compact_blank
+    params_check = {name: params[:name].gsub(/\s+/, ""), platform: params[:platform]}.compact_blank
 
     Game.all.each do |g|
       cond = true
@@ -35,6 +38,35 @@ class Api::GamesController < ActionController::API
     end
 
     render json: game_list.count > 0 ? game_list : Game.all
+  end
+
+  def populate 
+    urls = [
+      "https://interview-marketing-eng-dev.s3.eu-west-1.amazonaws.com/ios.top100.json",
+      "https://interview-marketing-eng-dev.s3.eu-west-1.amazonaws.com/android.top100.json"
+    ]
+
+    begin
+      urls.each do |url|
+        content = JSON.parse(Net::HTTP.get(URI.parse(url)))
+        content.each do |c|
+          Game.create(
+            publisher_id: c[0]['publisher_id'].to_s,
+            # encode to prevent bug when reading json later
+            name: c[0]['name'].force_encoding("ISO-8859-1").encode("UTF-8"),
+            platform: c[0]['os'].force_encoding("ISO-8859-1").encode("UTF-8"),
+            store_id: nil,
+            bundle_id: c[0]['bundle_id'],
+            app_version: c[0]['version'],
+            is_published: true
+          )
+        end
+      end
+      render json: Game.all
+
+    rescue => error
+      render json: error
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace "api" do
     resources "games", only: %i[index create update destroy] do
       collection do
+        get :populate
         post :search
       end
     end
